ad182709607dff9361cb1992f7632054cbf9571b
Tia Wagh <tiawagh@Tias-MacBook-Pro.local>
2020-02-28 15:25:14 -0500

    added java

---
+++ lab_assignments/one.js
@@ -0,0 +1,10 @@
+var title = document.getElementById("title")
+
+title.onmouseover =
+  function(){
+  title.style.color="red";
+}
+title.onmouseleave =
+  function(){
+  title.style.color="lightblue";
+}
