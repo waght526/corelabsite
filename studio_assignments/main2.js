91976de74902f1a49d8e63967678381a1859efdb
Tia Wagh <tiawagh@Tias-MacBook-Pro.local>
2020-04-28 06:41:23 -0700

    added minor changes

--- studio_assignments/main2.js
+++ studio_assignments/main2.js
@@ -1,24 +1,25 @@
 var objects = [
 	{
 		"name": "teddy",
-		"color": "white",
-		"size": "big",
-		"pic": "<img id='teddy' src='http://www.pngmart.com/files/10/Teddy-Bear-PNG-File.png' alt='teddy bear'>"
+		"location": "bed",
+		"description": "A big white teddy bear gifted to me by my best friend. Couldn't sleep without him.",
+		"pic": "<img id='teddy' src='http://www.pngmart.com/files/10/Teddy-Bear-PNG-File.png' alt='teddy bear'>",
+		"backgroundPic": "url(https://images-na.ssl-images-amazon.com/images/I/71FppCB1D-L._AC_SL1000_.jpg)"
 	},
 	{
 		"name": "medicine",
-		"color": "yellow",
-		"size": "small",
+		"location": "bathroom cabinet",
+		"description": "A medicine for headaches my mom used to give me as a child. Reminds me of home.",
 	    "pic": "<img id='medicine' src='https://i-cf5.gskstatic.com/content/dam/cf-consumer-healthcare/panadol/en_in/adult/productshots/crocinpainrelief/Crocin%20Pain%20Relief-Web%20Banner_455x455.png?auto=format' alt='medicine'>"
-
+        "backgroundPic": "url(https://images-na.ssl-images-amazon.com/images/I/71FppCB1D-L._AC_SL1000_.jpg)"
 	},
 	
 	{
 		"name": "hoodie",
-		"color": "grey",
-		"size": "big",
+		"location": "closet",
+		"description": "A soft hoodie I've been wearing for years that keeps me warm on cold days",
         "pic": "<img id='hoodie' src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngguru.com%2Ffree-transparent-background-png-clipart-nzcwn&psig=AOvVaw1iTOGoHJYE2YLVwA1Y5Utn&ust=1587698534669000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKjaoYvM_egCFQAAAAAdAAAAABAE' alt='hoodie'>"
-
+        "backgroundPic": "url(https://images-na.ssl-images-amazon.com/images/I/71FppCB1D-L._AC_SL1000_.jpg)"
 	}
 ]
 
@@ -43,6 +44,10 @@ var wallpapers = [
 
 $(document).ready(function(){
   $(".teddyb").click(function(){
-    $("#wallpaperbox").css('backgroundImage', 'url(https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.ch%2Fpin%2F591660469768371008%2F&psig=AOvVaw2tsBbNftXEC-shE5Df9eC2&ust=1587702668653000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKCK3L7b_egCFQAAAAAdAAAAABAE)');
+    $("#wallpaperbox").css({'background-image': objects[0].backgroundPic, 'background-repeat': repeat-y;});
+
   });
 });
+
+
+
